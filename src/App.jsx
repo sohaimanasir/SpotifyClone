1de7083c4383c8d9796a33f16ce5d16b39874196
Sohaima <sohaimanasir1@gmail.com>
2025-03-02 22:15:59 +0500

    cc

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import './App.css'
+import Header from './Header.jsx'
+import Sidebar from './Sidebar.jsx'
+import Preview from './Preview.jsx'
+
+function App() {
+
+  return (
+    <>
+<Header/>
+<Sidebar/>
+<Preview/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Preview.jsx
@@ -0,0 +1,20 @@
+import './Preview.css';
+
+function Preview() {
+    return (
+        <>
+            {/* Preview Section */}
+            <div className="preview-section">
+                <div className="preview-section-text">
+                <h3>Preview of Spotify</h3>
+                <p>Sign up to get unlimited songs and podcasts with occasional ads. No credit card needed.</p>
+                <button className="preview-btn">
+                    Sign up free
+                </button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Preview;
